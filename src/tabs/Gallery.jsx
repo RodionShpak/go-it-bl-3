0185f81d7265a2ad0eca68f8fa01a161669e0276
Veronika Savluk <s_veronica@ukr.net>
2022-10-15 12:15:15 +0300

    Update Gallery.jsx

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -8,23 +8,39 @@ export class Gallery extends Component {
     query: '',
     page: 1,
     images: [],
+    showButton: false,
   };
 
   hengleGetQuery = newQuery => {
-    this.setState({ query: newQuery });
+    this.setState({ query: newQuery, images: [], page: 1 });
   };
 
   componentDidUpdate(_, prevState) {
     const { query, page } = this.state;
     if (prevState.query !== query || prevState.page !== page) {
-      ImageService.getImages(query, page).then(data => console.log(data));
+      ImageService.getImages(query, page).then(data => this.setState(prevState => ({ images: [...prevState.images, ...data.photos], showButton: page < Math.ceil(data.total_results / 15) })));
     }
   }
+
+  handleClick = event => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  }
+
   render() {
+    const { images, showButton } = this.state;
+    const { handleClick } = this;
     return (
       <>
         <SearchForm onSubmit={this.hengleGetQuery} />
+        <Grid>
+          {images.length > 0 && images.map(({ id, alt, avg_color, src }) => (<GridItem key={id} >
+            <CardItem color={avg_color}>
+              <img src={src.large} alt={alt} />
+            </CardItem>
+            </GridItem>))}
+        </Grid>
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        {showButton && <Button onClick={handleClick}>Load more</Button>}
       </>
     );
   }

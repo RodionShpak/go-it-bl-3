f58013c1a1e7676fc0d03a30453b27ae336fe3b6
Nataliya Pogrebniak <pogrebnyak.nataliya@gmail.com>
2022-10-15 11:36:11 +0300

    add files

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,32 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+  hendleOnSubmit = event => {
+    event.preventDefault();
+    const {
+      elements: { search },
+    } = event.currentTarget;
+    const searchValue = search.value.trim().toLowerCase();
+    console.log(searchValue);
+    if (!searchValue) {
+      return;
+    }
+    this.props.onSubmit(searchValue);
+  };
+
   render() {
-    return <h2>SearchForm</h2>;
+    return (
+      <SearchFormStyled onSubmit={this.hendleOnSubmit}>
+        <FormBtn type="submit">
+          <FiSearch size="16px" />
+        </FormBtn>
+        <InputSearch
+          placeholder="What do you want to write?"
+          name="search"
+          required
+          autoFocus
+        />
+      </SearchFormStyled>
+    );
   }
 }
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -4,9 +4,26 @@ import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
+  state = {
+    query: '',
+    page: 1,
+    images: [],
+  };
+
+  hengleGetQuery = newQuery => {
+    this.setState({ query: newQuery });
+  };
+
+  componentDidUpdate(_, prevState) {
+    const { query, page } = this.state;
+    if (prevState.query !== query || prevState.page !== page) {
+      ImageService.getImages(query, page).then(data => console.log(data));
+    }
+  }
   render() {
     return (
       <>
+        <SearchForm onSubmit={this.hengleGetQuery} />
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
       </>
     );
